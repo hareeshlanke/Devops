bce7088353d7236d453726d17c453f59d09c815d
root <root@ip-172-31-30-2.ap-southeast-2.compute.internal>
2025-12-02 00:44:23 +0000

    python code committed

---
+++ app.py
@@ -0,0 +1,7 @@
+import array as arr
+
+my_array = arr.array('i', [200, 400, 600, 800, 900])
+
+print("Element at index=0 is : ", my_array[0])
+print("Element at index=1 is : ", my_array[1])
+print("Element at index=2 is : ", my_array[2])
